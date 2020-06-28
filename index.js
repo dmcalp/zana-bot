8ee1179c1d93f64cea355f7dba6063684a4341da
Dan <d-mcalpine@outlook.com>
2020-06-28 21:59:20 +0100

    server indepedent queue

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+/* eslint-disable brace-style */
 const Discord = require("discord.js");
 const { prefix, token, commands, d2APIkey } = require("./config.json");
 const ytdl = require("ytdl-core");
@@ -13,10 +14,9 @@ zana.once("ready", () => {
 
 let queue = [];
 const titles = [];
+const servers = {};
 
-
-zana.on("message", async message => {
-
+zana.on("message", async (message) => {
 	if (!message.guild) return;
 	if (!message.content.startsWith(prefix) || message.author.bot) return;
 
@@ -36,141 +36,149 @@ zana.on("message", async message => {
 					const video = results.videos[0];
 					titles.push(`${video.title} **[${video.timestamp}]**`);
 					queue.push(video.url);
-					const resp = (message.guild.voiceConnection)
+					const resp = message.guild.voiceConnection
 						? `${video.title} **[${video.timestamp}]** added to the queue.`
 						: `${video.title} **[${video.timestamp}]** is now playing.`;
 					if (message.guild.voiceConnection) {
-						message.reply(resp + "\n **Current queue:** \n" + titles.join("\n"));
+						message.channel.send(`${resp}\n\n**Current queue:**\n${titles.join("\n")}`);
 					}
 					else {
-						message.reply(resp);
+						message.channel.send(resp);
 					}
 				}
 				else {
-					console.log("No videos found.");
+					message.reply("Nothing was found, try again.");
 					return;
 				}
 			}
 			if (!message.guild.voiceConnection) {
+				if (!servers[message.guild.id]) {
+					servers[message.guild.id] = { queue: [] };
+				}
 				message.member.voiceChannel.join().then((connection) => {
 					play(connection, message);
 				});
 			}
-
 		}
 		else {
 			return message.reply("You must be in a voice channel to use this!");
 		}
-	}
 
-	else if (command === "skip") {
+	} else if (command === "skip") {
 		if (message.guild.voiceConnection) {
 			dispatcher.end();
-		}
-		else {
+		} else {
 			message.reply("I must be in a voice channel in order to skip!");
 		}
 
-	}
-
-	else if (command === "volume") {
-		if (!message.guild.voiceConnection) return message.reply("I must be in a voice channel for this command!");
+	} else if (command === "volume") {
+		if (!message.guild.voiceConnection) {
+			return message.reply("I must be in a voice channel for this command!");
+		}
 		const vol = args[0];
-		vol >= 0.1 && vol <= 1 ? dispatcher.setVolume(vol) : message.reply("Volume must be 0.1 - 1");
-	}
+		vol >= 0.1 && vol <= 1
+			? dispatcher.setVolume(vol)
+			: message.reply("Volume must be 0.1 - 1");
 
-	else if (command === "pause") {
-		if (!message.guild.voiceConnection) return message.reply("I must be in a voice channel for this command!");
+	} else if (command === "pause") {
+		if (!message.guild.voiceConnection) {
+			return message.reply("I must be in a voice channel for this command!");
+		}
 		if (!dispatcher.paused) {
 			dispatcher.pause();
 			message.reply("Track paused, use `!pause` again to resume.");
-		}
-		else {
+		} else {
 			dispatcher.resume();
 		}
-	}
 
-	else if (command === "leave") {
+	} else if (command === "leave") {
 		if (message.guild.voiceConnection) {
 			message.guild.voiceConnection.disconnect();
 			queue = [];
-		}
-		else {
+		} else {
 			message.reply("I must be in a voice channel in order to leave!");
 		}
-	}
 
-	else if (command === "choose") {
-		const options = message.content.slice(prefix.length + command.length).split(",");
-		const trimmedOptions = options.map(s => s.trim());
-		const choice = trimmedOptions[Math.floor(Math.random() * trimmedOptions.length)];
+	} else if (command === "choose") {
+		const options = message.content
+			.slice(prefix.length + command.length)
+			.split(",");
+		const trimmedOptions = options.map((s) => s.trim());
+		const choice =
+			trimmedOptions[Math.floor(Math.random() * trimmedOptions.length)];
 		message.channel.send(`I choose: ${choice}.`);
-	}
 
-	else if (command === "d2time") {
-		const data = await fetch("https://www.bungie.net/Platform/Destiny2/3/Profile/4611686018467716166/Character/2305843009404638901/?components=200", {
-			headers: {
-				"X-API-Key": d2APIkey,
-			},
-		}).then(response => response.json());
+	} else if (command === "d2time") {
+		const data = await fetch(
+			"https://www.bungie.net/Platform/Destiny2/3/Profile/4611686018467716166/Character/2305843009404638901/?components=200",
+			{
+				headers: {
+					"X-API-Key": d2APIkey,
+				},
+			}
+		).then((response) => response.json());
 
-		const msg = "Account 'D4NDK' has " + Math.floor(data.Response.character.data.minutesPlayedTotal / 60) + " hours played across all platforms.";
+		const msg =
+			"Account 'D4NDK' has " +
+			Math.floor(data.Response.character.data.minutesPlayedTotal / 60) +
+			" hours played across all platforms.";
 		message.channel.send(msg);
-	}
 
-	else if (command === "urban") {
+	} else if (command === "urban") {
 		if (!args.length) {
 			return message.channel.send("Usage: !urban word");
 		}
 		const query = args.join("%20");
-		const { list } = await fetch(`https://api.urbandictionary.com/v0/define?term=${query}`).then(response => response.json());
+		const { list } = await fetch(
+			`https://api.urbandictionary.com/v0/define?term=${query}`
+		).then((response) => response.json());
 		const definition = list[0].definition;
 		const example = list[0].example;
 		const thumbsup = list[0].thumbs_up;
 		const thumbsdown = list[0].thumbs_down;
-		message.channel.send(`**Definition:**\n${definition}\n\n**Example:**\n${example}\n\n**Thumbs Up:** ${thumbsup}  **Thumbs Down:** ${thumbsdown}`);
-	}
+		message.channel.send(
+			`**Definition:**\n${definition}\n\n**Example:**\n${example}\n\n**Thumbs Up:** ${thumbsup}  **Thumbs Down:** ${thumbsdown}`
+		);
 
-	else if (command === "server") {
-		message.channel.send(`The server '${message.guild.name}' was created on ${message.guild.createdAt}`);
-	}
+	} else if (command === "server") {
+		message.channel.send(
+			`The server '${message.guild.name}' was created on ${message.guild.createdAt}`
+		);
 
-	else if (command === "avatar") {
+	} else if (command === "avatar") {
 		message.channel.send(message.author.displayAvatarURL);
-	}
 
-	else if (command === "queue") {
-		const response = (queue.length) ? queue : "The queue is currently empty.";
+	} else if (command === "queue") {
+		const response = queue.length ? queue : "The queue is currently empty.";
 		message.reply(response);
-	}
 
-	else if (command === "ping") {
+	} else if (command === "ping") {
 		message.channel.send(`Zana's ping is currently: ${message.client.ping}`);
-	}
 
-	else if (command === "delete") {
+	} else if (command === "delete") {
 		if (args.length > 0) {
 			if (args[0] > 1 && args[0] <= 10) {
 				const amount = parseInt(args[0]) + 1;
-				message.channel.bulkDelete(amount).catch(err => {
+				message.channel.bulkDelete(amount).catch((err) => {
 					console.error(err);
 					message.reply("Error deleting messages!");
 				});
-			}
-			else {
+			} else {
 				message.reply("The second argument must be 2-10 inclusive");
 			}
+		} else {
+			message.reply(
+				"This method requires an additional argument (2-10 inclusive)"
+			);
 		}
-		else {
-			message.reply("This method requires an additional argument (2-10 inclusive)");
-		}
-	}
 
-	else if (command === "commands") {
+	} else if (command === "commands") {
 		const embed = new Discord.RichEmbed()
 			.setTitle("Zana Commands")
 			.setColor("#e60965")
-			.setThumbnail("https://pngimage.net/wp-content/uploads/2018/06/lenny-png-7.png")
+			.setThumbnail(
+				"https://pngimage.net/wp-content/uploads/2018/06/lenny-png-7.png"
+			)
 			.setDescription(commands)
 			.setTimestamp();
 		message.channel.send(embed);
@@ -178,25 +186,25 @@ zana.on("message", async message => {
 });
 
 function play(connection, message) {
-	if (queue[0]) {
+	const server = servers[message.guild.id];
+	if (server.queue[0]) {
 		zana.user.setActivity(titles.shift());
-		dispatcher = connection.playStream(ytdl(
-			queue.shift(), {
+		server.dispatcher = connection.playStream(
+			ytdl(queue.shift(), {
 				filter: "audioonly",
 				bitrate: 64000,
 				highWaterMark: 1 << 25,
-			}));
-		dispatcher.setVolume(0.2);
-		dispatcher.on("end", () => {
-			if (queue[0]) {
+			})
+		);
+		server.dispatcher.setVolume(0.2);
+		server.dispatcher.on("end", () => {
+			if (server.queue[0]) {
 				play(connection, message);
-			}
-			else {
+			} else {
 				connection.disconnect();
 				zana.user.setActivity();
 			}
 		});
 	}
 }
-
 zana.login(token);
