3a73ffcb153c530a832761807bc9e22efb6f2b04
Dan <d-mcalpine@outlook.com>
2020-09-06 03:08:29 +0100

    remove severs debug command

--- index.js
+++ index.js
@@ -283,8 +283,6 @@ zana.on('message', async (message) => {
 			.setDescription(commands)
 			.setTimestamp();
 		message.channel.send(embed);
-	} else if (command === 'servers') {
-		console.log(servers);
 	}
 });
 
