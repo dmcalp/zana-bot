a62faecf1de658236832a9d94805b0ce274bafa1
Dan <d-mcalpine@outlook.com>
2021-09-27 18:32:56 +0100

    removed commented out alt code from debugging

--- commands/leave.js
+++ commands/leave.js
@@ -6,7 +6,6 @@ module.exports = {
   execute(message, args, servers) {
     const server = servers[message.guild.id];
     if (message.guild.voice.connection) {
-      // message.guild.voice.connection.disconnect();
       server.queue = [];
       skip.execute(message, args, servers);
     } else {
