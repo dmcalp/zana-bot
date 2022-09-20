7f470fa2d50fd1cfb4af2e41cf9edf4b2c2ce7ba
Daniel McAlpine <d-mcalpine@outlook.com>
2022-09-20 20:24:54 +0100

    error handling

---
+++ commands/skip.js
@@ -0,0 +1,19 @@
+const { SlashCommandBuilder } = require('discord.js');
+const { getVoiceConnection } = require('@discordjs/voice');
+
+module.exports = {
+    data: new SlashCommandBuilder()
+        .setName('skip')
+        .setDescription('Skips the current track'),
+
+    async execute(interaction, servers) {
+        const server = servers[interaction.guild.id];
+
+        if (getVoiceConnection(interaction.guild.id) !== undefined) {
+            server.audioPlayer.stop(true);
+            interaction.reply('Skipping track..')
+        } else {
+            interaction.reply({content: 'Skip failed - no media playing.', ephemeral: true});
+        }
+    },
+}
