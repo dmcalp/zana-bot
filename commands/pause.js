0ba9ac7e88f60b0d790c640cb703176ac4227d7c
Daniel McAlpine <d-mcalpine@outlook.com>
2022-09-20 20:23:57 +0100

    error handling

--- commands/pause.js
+++ commands/pause.js
@@ -1,4 +1,5 @@
 const { SlashCommandBuilder } = require('discord.js');
+const { getVoiceConnection } = require('@discordjs/voice');
 
 module.exports = {
     data: new SlashCommandBuilder()
@@ -7,15 +8,18 @@ module.exports = {
 
     async execute(interaction, servers) {
         const server = servers[interaction.guild.id];
-        let action = "An error occurred.";
+        let action = "Pause failed - no media playing.";
         
-        if (server.audioPlayer._state.status == 'playing') {
-            server.audioPlayer.pause();
-            action = "Track paused.";
-        }
-        else if (server.audioPlayer._state.status == 'paused') {
-            server.audioPlayer.unpause();
-            action = "Track resumed.";
+        if (getVoiceConnection(interaction.guild.id) !== undefined) {
+        
+            if (server.audioPlayer._state.status == 'playing') {
+                server.audioPlayer.pause();
+                action = "Track paused.";
+            }
+            else if (server.audioPlayer._state.status == 'paused') {
+                server.audioPlayer.unpause();
+                action = "Track resumed.";
+            }
         }
         
         interaction.reply(action);
