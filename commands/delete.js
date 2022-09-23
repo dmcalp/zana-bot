c1491285a10cf432ef4612153daa7fda8c419167
Daniel McAlpine <d-mcalpine@outlook.com>
2022-09-23 21:56:56 +0100

    bulk delete slash command added

---
+++ commands/delete.js
@@ -0,0 +1,20 @@
+const { SlashCommandBuilder } = require('discord.js');
+
+module.exports = {
+    
+    data: new SlashCommandBuilder()
+        .setName('delete')
+        .setDescription('Bulk delete 1-10 messages')
+        .addIntegerOption(option => option.setName('input').setDescription('A number from 1 - 10 inclusive').setRequired(true)),
+
+    async execute(interaction) {
+        
+        const delAmount = interaction.options.getInteger('input');
+        
+        if (delAmount > 0 && delAmount <= 10) {
+            interaction.channel.bulkDelete(delAmount)
+                .then(messages => interaction.reply(`Bulk deleted ${messages.size} messages.`))
+                .catch(console.error);
+        }
+    }
+}
